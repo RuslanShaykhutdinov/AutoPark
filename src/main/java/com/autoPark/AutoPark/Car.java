960f1f24805c23f7ba6d152bbd1c42d00a21a076
SanjarU1337 <ulmasbaevsanjar@gmail.com>
2022-03-12 22:30:28 +0500

    Car entity

---
+++ src/main/java/com/autoPark/AutoPark/Car.java
@@ -0,0 +1,38 @@
+package com.autoPark.AutoPark;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+
+    private String carId;
+    private Object category;
+    private Long driverId = null;
+
+    public Car() {
+    }
+
+    public Car(String carId, Object category) {
+        carId = carId;
+        category = category;
+    }
+
+    public Car(String carId, Object category, long driverId) {
+        carId = carId;
+        category = category;
+        driverId = driverId;
+    }
+    public Boolean isOwned(){
+        if(driverId == 0)
+            return false;
+        else{
+            return true;
+        }
+    }
+}
